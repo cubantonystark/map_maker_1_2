4a5ddac4ad63b30b37f6149ca26fb2c6a9cffa5f
Cuban Tony Stark <escambray@outlook.com>
2023-07-14 04:16:44 -0400

    Update MM_GUI.py
    Added a Progress Bar to the Surface Recon to indicate its running, on top of disabling the Browse Button

--- MM_GUI.py
+++ MM_GUI.py
@@ -1,11 +1,10 @@
 import win32gui, win32con
 '''
 This snippet hides the console in non compiled scripts. Done for aesthetics
-
+'''
 
 this_program = win32gui.GetForegroundWindow()
 win32gui.ShowWindow(this_program , win32con.SW_HIDE)
-'''
 
 import random, psutil
 from datetime import datetime
@@ -69,7 +68,7 @@ subprocess.Popen(["python", "MM_loop_check_files.py"])
 r = random.Random()
 session_id = r.randint(1, 10000000)
 session_logger = MM_logger.initialize_logger("SessionLog" + str(session_id))
-#print = session_logger.info
+print = session_logger.info
 
 class SdCardInsertionEvent(tk.Event):
     def __init__(self, drive_letter):
