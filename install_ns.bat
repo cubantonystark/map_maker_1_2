d69965788a54d2810dd1496d3ce5ea234725bbb3
Cuban Tony Stark <escambray@outlook.com>
2023-07-20 21:33:34 -0400

    Added requirements for torch and cuda 11.8 as well as added the button for processing datasets using Neural Radiance Fields

---
+++ install_ns.bat
@@ -0,0 +1,8 @@
+pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+git clone https://github.com/nerfstudio-project/nerfstudio.git
+cd nerfstudio
+pip install --upgrade pip setuptools
+pip install -e .
+pip install nerfstudio
+cd ..
