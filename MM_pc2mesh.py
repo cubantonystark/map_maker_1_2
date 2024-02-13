dc7933dffc36d0db87b2df5116327eb9d2cface0
cubantonystark <escambray@outlook.com>
2024-02-13 12:29:41 -0500

    Fine tuned mesh refinement values

--- MM_pc2mesh.py
+++ MM_pc2mesh.py
@@ -317,6 +317,7 @@ class meshing():
 
                 boundingbox = ms.current_mesh().bounding_box()
                 diag = boundingbox.diagonal()
+
                 t_hold = diag / 200
 
                 p = pymeshlab.PercentageValue(25)
@@ -342,7 +343,7 @@ class meshing():
                 else:
                     t_hold = 0.1
 
-                    # Since there will still be some long faces, we will mark them and remove them, this time applying a 0.06 thershold. This is
+                # Since there will still be some long faces, we will mark them and remove them, this time applying a 0.06 thershold. This is
 
                 ms.apply_filter('compute_selection_by_edge_length',
                                 threshold=t_hold)
@@ -374,13 +375,17 @@ class meshing():
                     # logging.info('Mesh not optimal. Retargeting parameters (1).\r')
                     message = 'Mesh not optimal. Retargeting parameters (1).'
                     self.write_to_log(path, separator, message)
+
                     boundingbox = ms.current_mesh().bounding_box()
                     diag = boundingbox.diagonal()
                     t_hold = diag / 200
-                    p = pymeshlab.PercentageValue(25)
+
+                    p = pymeshlab.PercentageValue(10)
+
                     # logging.info('Refining.\r')
                     message = 'Refining'
                     self.write_to_log(path, separator, message)
+
                     # We will select faces that are long based on the bounding box calculation and then remove them
                     ms.apply_filter('compute_selection_by_edge_length',
                                     threshold=t_hold)
@@ -391,13 +396,13 @@ class meshing():
                                     mincomponentdiag=p)
 
                     if ".ply" in filename:
-                        t_hold = 0.2
+                        t_hold = 0.3
 
                     elif ".pts" in filename:
                         t_hold = 0.095
 
                     else:
-                        t_hold = 0.2
+                        t_hold = 0.3
 
                     # Since there will still be some long faces, we will mark them and remove them, this time applying a 0.06 thershold. This is
                     ms.apply_filter('compute_selection_by_edge_length',
@@ -447,13 +452,13 @@ class meshing():
                                         mincomponentdiag=p)
 
                         if ".ply" in filename:
-                            t_hold = 0.3
+                            t_hold = 0.6
 
                         elif ".pts" in filename:
                             t_hold = 0.099
 
                         else:
-                            t_hold = 0.3
+                            t_hold = 0.6
 
                         # Since there will still be some long faces, we will mark them and remove them, this time applying a 0.06 thershold. This is
                         ms.apply_filter('compute_selection_by_edge_length',
